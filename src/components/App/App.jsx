4a2f8c547c9f4fcf96f34807718610c656631173
melodyn <samelodyn@gmail.com>
2022-10-31 18:01:52 +0300

    #1 prepare file structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +24,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
