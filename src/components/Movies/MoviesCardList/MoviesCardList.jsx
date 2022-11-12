5b457b0e50c323db5524d8d9ece8b248f8124de7
melodyn <samelodyn@gmail.com>
2022-11-12 10:25:53 +0300

    #1 layouts movies list

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,4 +1,5 @@
 import './MoviesCardList.css';
+import { Preloader } from '../Preloader/Preloader';
 import { MoviesCard } from '../MoviesCard/MoviesCard';
 import pepe from '../../../images/pepe-photo.png';
 import save from '../../../images/save.svg';
@@ -11,7 +12,7 @@ const inc = (() => {
   };
 })();
 
-const defcards = [
+const fullCards = [
   {
     id: inc(),
     name: 'Лучшие мемы 2022',
@@ -55,11 +56,50 @@ const defcards = [
     saved: true,
   },
 ];
+const emptyCards = [];
+const loadingCards = [null];
+const defaultCards = (() => {
+  const cards = [fullCards, emptyCards, loadingCards];
+  let i = 0;
+  return () => {
+    i += 1;
+    if (i >= cards.length) {
+      i = 0;
+    }
+    return cards[i];
+  };
+})();
 
-export const MoviesCardList = ({ cards = defcards }) => (
-  <article className="article" aria-label="Все роллы">
-    <ul className="cards">
-      {cards.map((card) => <MoviesCard key={card.id} card={card} />)}
-    </ul>
-  </article>
-);
+export const MoviesCardList = ({ cards = defaultCards() }) => {
+  const isEmpty = cards.length === 0;
+  const isLoading = !isEmpty
+    && (cards.length === 1)
+    && (cards[0] === null);
+  const hasCards = !isEmpty && !isLoading;
+
+  return (
+    <article className="article movies" aria-label="Все роллы">
+      {hasCards && (
+      <>
+        <ul className="cards">
+          {cards.map((card) => <MoviesCard key={card.id} card={card} />)}
+        </ul>
+        <button
+          type="button"
+          className="animation button movies__more"
+          disabled={isLoading}
+        >
+          Ещё
+        </button>
+      </>
+      )}
+      {isLoading && (
+      <>
+        <Preloader />
+        <p className="movies__load-info">Загрузка...</p>
+      </>
+      )}
+      {isEmpty && <p className="movies__load-info">Ничего не найдено</p>}
+    </article>
+  );
+};
